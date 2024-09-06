c4bd29654b54a4acde295f26e9140cd270d46a99
Gabi da Silva <gabriela.silva.07.11@gmail.com>
2024-09-06 16:18:18 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Transtorno do Espectro Autista (TEA)",
+        descricao: "Caracterizado por dificuldades na comunicação social, padrões de comportamento repetitivos e interesses restritos.O TEA é uma condição complexa e heterogênea, que afeta a forma como uma pessoa se comunica, interage socialmente e percebe o mundo ao seu redor. O termo espectro enfatiza a ampla gama de sintomas e gravidades que podem ser observadas entre as pessoas com TEA.Características do TEA são dificuldades na comunicação social, padrões de comportamento repetitivos e restritos e dificuldades na interação social.",
+        link: "https://orienteme.com.br/blog/neurodivergente/",
+        tags: "tea autismo espectro"
+    },
+    {
+        titulo: "Transtorno do Déficit de Atenção e Hiperatividade (TDAH)",
+        descricao: "O Transtorno do Déficit de Atenção com Hiperatividade (TDAH) é um distúrbio neurobiológico crônico que se caracteriza por um padrão persistente de desatenção e/ou hiperatividade-impulsividade, mais intenso e frequente do que o normalmente observado em outras crianças da mesma idade e desenvolvimento. Características do TDAH são desatenção, hiperatividade e impulsividade",
+        link: "https://bvsms.saude.gov.br/transtorno-do-deficit-de-atencao-com-hiperatividade-tdah/#:~:text=É%20um%20transtorno%20neurobiológico%20de,de%20atenção%2C%20inquietação%20e%20impulsividade.a",
+        tags: "tdah déficit hiperatividade atenção"
+    },
+    {
+        titulo: "Síndrome de Tourette",
+        descricao: "A Síndrome de Tourette é um distúrbio neurológico caracterizado por tiques motores e vocais involuntários e repetitivos. Embora seja mais conhecida pelos tiques, a síndrome pode apresentar uma variedade de outros sintomas e desafios.",
+        link: "https://pt.wikipedia.org/wiki/Síndrome_de_Tourette",
+        tags: "tourette movimentos involuntários tiques"
+    }
+];
---
+++ read.md
@@ -0,0 +1,14 @@
+# Projeto Imersão Alura e Gemini #
+
+**Descrição da Imersão**
+
+:arrow_right:A Imersão Alura e Google Gemini foi mais um desafio importante em minha jornada e super gratificante! Mergulhei no mundo da IA e aprendi a lidar com a complexidade da Gemini API. No final, consegui construir uma página  web do zero, o que me deixou muito orgulhosa.
+:arrow_right:Gostaria de agradecer à Alura e ao Google por essa oportunidade incrível! A Imersão me proporcionou um rico aprendizado e a Alura, como sempre, me conectando com uma comunidade incrível de desenvolvedores via Discord. Graças a essa experiência, consegui fazer uma página de pesquisa acerca de Neurodivergências.
+
+:gear: Ferramentas
+    1. HTML5;
+    2. CSS;
+    3. JavaScript;
+    4. VS Code;
+    5. GitHub, e;
+    6. Vercel.
